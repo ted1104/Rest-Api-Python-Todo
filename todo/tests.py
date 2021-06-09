c7b53bf519d0ed0e3f13cb19b11f46406842001f
Teddy Walter <teddywalter2016@gmail.com>
2021-06-09 11:55:33 +0100

    wrtingn test for create todo

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,34 @@
+from datetime import datetime
+
 from django.test import TestCase
+from todo.models import Todo, TodoList
+
 
 # Create your tests here.
+class TodoTestCase(TestCase):
+    def setUp(self):
+        self.todoList = TodoList()
+        self.todoList.name = 'Test new to do list'
+        self.todoList.save()
+
+
+    def test_create_todo(self):
+        # voir combien sont presents dans la db#
+        # ajouter un objet dans notre DB#
+        # Ajouter un object dans notre DB
+        # Valider que le nombre d'object dans notre db a ete incremente de 1
+
+        nbr_of_todo_before_add = Todo.objects.count()
+        new_todo = Todo()
+        new_todo.title = "Acheter un stylo"
+        new_todo.due_date = datetime.today()
+        new_todo.favorite = True
+        new_todo.completed = True
+        new_todo.id_list = self.todoList
+
+        new_todo.save()
+
+        nbr_of_todo_after_add = Todo.objects.count()
+
+        self.assertTrue(nbr_of_todo_after_add == nbr_of_todo_before_add+1)
+
